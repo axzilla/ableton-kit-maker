2d6c5e1e9dbe5b3efc41c760e4e901fc90ea5dde
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-07-17 15:18:54 +0200

    Increase app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from './logo.png'
 import './App.css'
 
 const { dialog } = window.require('electron').remote
+const fs = window.require('fs')
+const path = require('path')
+
 function App() {
+  function saveFile() {
+    dialog
+      .showSaveDialog({
+        title: 'Select the File Path to save',
+        defaultPath: path.join(__dirname, '../assets/sample.txt'),
+        buttonLabel: 'hjkh',
+        // Restricting the user to only Text Files.
+        filters: [
+          {
+            name: 'Text Files',
+            extensions: ['txt', 'docx'],
+          },
+        ],
+        properties: [],
+      })
+      .then((file) => {
+        // Stating whether dialog operation was cancelled or not.
+        console.log(file.canceled)
+        if (!file.canceled) {
+          console.log(file.filePath.toString())
+
+          // Creating and Writing to the sample.txt file
+          fs.writeFile(
+            file.filePath.toString(),
+            'This is a Sample File',
+            function (err) {
+              if (err) throw err
+              console.log('Saved!')
+            }
+          )
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Drag & Drop Maschine Kit Folders</p>
+        <code>or</code>
         <p
-          onClick={() =>
-            dialog.showOpenDialog({
-              properties: ['openFile', 'multiSelections'],
-            })
-          }
-        >
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          onClick={saveFile}
+          style={{ pointer: 'cursor' }}
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Select Folders
+        </p>
       </header>
     </div>
   )
