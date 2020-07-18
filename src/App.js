93951bcb0dd667a9bc335d1cec4c9613b48060c5
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-07-18 08:44:55 +0200

    Remove standart CRA css styles

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
 import React from 'react'
-import logo from './logo.png'
-import './App.css'
 
 const { dialog } = window.require('electron').remote
 const fs = window.require('fs')
 const path = require('path')
 
 function App() {
-  function saveFile() {
-    dialog
-      .showSaveDialog({
-        title: 'Select the File Path to save',
-        defaultPath: path.join(__dirname, '../assets/sample.txt'),
-        buttonLabel: 'hjkh',
-        // Restricting the user to only Text Files.
-        filters: [
-          {
-            name: 'Text Files',
-            extensions: ['txt', 'docx'],
-          },
-        ],
-        properties: [],
+  async function handleOpenFolder() {
+    await dialog
+      .showOpenDialog({
+        properties: ['openDirectory', 'multiSelections'],
       })
-      .then((file) => {
-        // Stating whether dialog operation was cancelled or not.
-        console.log(file.canceled)
-        if (!file.canceled) {
-          console.log(file.filePath.toString())
-
-          // Creating and Writing to the sample.txt file
-          fs.writeFile(
-            file.filePath.toString(),
-            'This is a Sample File',
-            function (err) {
-              if (err) throw err
-              console.log('Saved!')
-            }
-          )
-        }
-      })
-      .catch((err) => {
-        console.log(err)
+      .then((result) => {
+        console.log(result)
       })
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Drag & Drop Maschine Kit Folders</p>
+        <p>Drag & Drop</p>
         <code>or</code>
         <p
-          onClick={saveFile}
+          onClick={handleOpenFolder}
           style={{ pointer: 'cursor' }}
           className="App-link"
         >
-          Select Folders
+          Select Expansion Pack(s)
         </p>
       </header>
     </div>
