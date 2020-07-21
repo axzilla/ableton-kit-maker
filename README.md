10e905b6dc689aa86d5dda28ccee659da400dd8b
Axel Adrian <mail@badazz.dev>
2020-07-21 16:28:54 +0000

    Resolve "Electron Build does not work"

---
+++ README.md
@@ -0,0 +1,1 @@
+Add `SKIP_PREFLIGHT_CHECK=true` to a `.env` file because of a eslint version error while starting react server in dev mode.
--- public/electron.js
+++ public/electron.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev')
 
-require('./mains/getExtensionsMain')
+require('./../mains/getExtensionsMain')
 
 let mainWindow
 
@@ -16,9 +16,7 @@ function createWindow() {
   })
 
   mainWindow.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`,
   )
 
   mainWindow.on('closed', () => (mainWindow = null))
