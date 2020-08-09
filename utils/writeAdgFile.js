711018f6462cc5c32823c3d76498a0a78f11ae3f
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-08-09 21:06:57 +0200

    Refactoring the hole createAdg setup

--- utils/writeAdgFile.js
+++ utils/writeAdgFile.js
@@ -3,14 +3,17 @@ const path = require('path')
 
 const { getUserLib } = require('./getUserLib')
 
-async function writeAdgFile(expansionName, kitName, padString) {
-  const headerPath = path.join(__dirname, '..', 'ejs', 'header.ejs')
-  const footerPath = path.join(__dirname, '..', 'ejs', 'footer.ejs')
-
-  const headerString = await fsx.readFile(headerPath, 'UTF-8')
-  const footerString = await fsx.readFile(footerPath, 'UTF-8')
-
-  const concatedAdg = headerString.concat(padString, footerString)
+async function writeAdgFile(expansionName, kitName, sample) {
+  await fsx.ensureDir(
+    path.join(
+      getUserLib(),
+      'Presets',
+      'Instruments',
+      'Drum Rack',
+      'Ableton Kit Maker',
+      expansionName,
+    ),
+  )
 
   await fsx.writeFile(
     path.join(
@@ -22,7 +25,7 @@ async function writeAdgFile(expansionName, kitName, padString) {
       expansionName,
       `${kitName}.adg`,
     ),
-    concatedAdg,
+    sample,
     'UTF-8',
   )
 }
