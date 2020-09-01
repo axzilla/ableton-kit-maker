2db4db7b6328a55a9c308b8c6b24cdedf642179d
‘axzilla’ <chanter-acidic-0r@icloud.com>
2020-09-01 11:20:25 +0200

    Remove google tracking id and add the new one to .env

--- mains/handleAnalyticsMain.js
+++ mains/handleAnalyticsMain.js
@@ -5,8 +5,9 @@ const getMAC = require('getmac')
 
 const handleAnalyticsMain = ipcMain.handle('handle-analytics', async (event, data) => {
   try {
+    const googleTrackingId = process.env.GOOGLE_TRACKING_ID
     const macAdress = await getMAC.default()
-    var visitor = ua('UA-154947065-4', macAdress, { strictCidFormat: false })
+    var visitor = ua(googleTrackingId, macAdress, { strictCidFormat: false })
     visitor.pageview(data).send()
   } catch (error) {
     if (error) throw error
